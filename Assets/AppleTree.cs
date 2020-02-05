58d6088b12a9cbac3fc6207dbd7559efee829ee4
JustinZFEng <60244248+JustinZFEng@users.noreply.github.com>
2020-02-05 14:08:14 -0500

    Added basic movement

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set	in	Inspector")]
+    
+    public GameObject applePrefab;
+    
+    public float speed = 1f;
+    
+    public float leftAndRightEdge = 20;
+    
+    public float chanceToChangeDirections = 0.1f;
+    
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
