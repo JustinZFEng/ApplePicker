40f802d3062f256f0e0bbe02db8137c83583283f
JustinZFEng <60244248+JustinZFEng@users.noreply.github.com>
2020-02-05 15:26:17 -0500

    Added Falling Apples

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 public class AppleTree : MonoBehaviour
 {
-    [Header("Set	in	Inspector")]
+    [Header("Set in Inspector")]
     
     public GameObject applePrefab;
     
     public float speed = 1f;
     
-    public float leftAndRightEdge = 20;
+    public float leftAndRightEdge = 10f;
     
     public float chanceToChangeDirections = 0.1f;
     
@@ -17,8 +17,16 @@ public class AppleTree : MonoBehaviour
 
     void Start()
     {
-        
+        Invoke("DropApple", 2f);                                                                                                                                                      
     }
+
+    void DropApple()
+    {                                                                                                                                                                                                       
+        GameObject apple = Instantiate<GameObject>(applePrefab);                     
+        apple.transform.position = transform.position;                                                                     
+        Invoke("DropApple", secondsBetweenAppleDrops);                                                            
+    }
+
     void Update()
     {
         Vector3 pos = transform.position;
